76d0289cf82fbc1b7ca1b8d81fcaf3b81c08a0b2
Balint Soos <soosbalint95@gmail.com>
2016-09-18 05:13:43 +0200

    delete useless modules

--- src/utils/parseId/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint no-param-reassign: ["error", { "props": false }] */
-
-function parseId(req, res, next) {
-  req.params.id = parseInt(req.params.id, 10)
-
-  next()
-}
-
-module.exports = parseId
